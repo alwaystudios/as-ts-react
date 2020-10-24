6819681afe0384c07a445c65e33d916db217cb53
Gary Alway <gary.alway@acuris.com>
2020-10-24 01:07:54 +0100

    useThrottledRequests demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,59 @@
-import { useAsync } from "@alwaystudios/as-ui-components";
-import { promiseTimeout } from "@alwaystudios/as-utils";
-import React from "react";
-import { useEffect } from "react";
-import { Header } from "./components/Header";
-import { State } from "./components/State";
+import { useThrottledRequests, useAsync } from "@alwaystudios/as-ui-components";
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  const { state, callback } = useAsync(async () => {
-    await promiseTimeout(2000);
-    return "test";
+  const [parallel, setParallel] = useState<number>(10);
+  const [requestCount, setRequestCount] = useState<number>(1000);
+  const { throttleActions, throttleProgress } = useThrottledRequests(parallel);
+
+  const requestsToMake = Array.from(Array(requestCount)).map(
+    (_, index) => async () => {
+      try {
+        const response = await fetch(
+          `/manifest.json?querystringValueToPreventCaching=${new Date()}_request-${index}`
+        );
+        const json = await response.json();
+
+        throttleActions.requestSucceededWithData(json);
+      } catch (error) {
+        throttleActions.requestFailedWithError(error);
+      }
+    }
+  );
+
+  const { callback } = useAsync(async () => {
+    await throttleActions.queueRequests(requestsToMake);
   });
 
-  useEffect(() => {
-    if (!state.called) {
-      callback();
-    }
-  }, [callback, state]);
-
   return (
-    <>
-      <Header />
-      <State state={state} />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <h1>useThrottledRequests</h1>
+        Max parallel:
+        <input
+          value={parallel}
+          onChange={(event) => setParallel(Number(event.currentTarget.value))}
+        />
+        Requests:
+        <input
+          value={requestCount}
+          onChange={(event) =>
+            setRequestCount(Number(event.currentTarget.value))
+          }
+        />
+        <button onClick={callback}>Fire</button>
+        {throttleProgress.loading && <div>Loading</div>}
+        {throttleProgress.values.length > 0 && (
+          <div>
+            {throttleProgress.values.length} requests completed successfully
+          </div>
+        )}
+        {throttleProgress.errors.length > 0 && (
+          <div>{throttleProgress.errors.length} requests errored</div>
+        )}
+      </header>
+    </div>
   );
 };
 
